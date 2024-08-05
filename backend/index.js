185d99053d785d07e00b8cb8c1c2de5c4175c75c
Shreyask24 <shreyask495@gmail.com>
2024-08-05 13:56:11 +0530

    Correted backend errors

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = ` https://image-generator-xofb.onrender.com`;
 
 app.use(cors())
 app.use(bodyParser.json());
@@ -15,7 +15,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, '../img-generate/dist')));
 
 
-app.post('/api/generate-og-image', async (req, res) => {
+app.post('https://image-generator-xofb.onrender.com/api/generate-og-image', async (req, res) => {
   const { title, content } = req.body;
 
   try {
@@ -58,5 +58,5 @@ app.get('*', (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+  console.log(`Server is running on https://image-generator-xofb.onrender.com`);
 });
